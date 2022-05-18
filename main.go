50a89386f3c67be9f1d169c750e67c75994b23cf
Joonkyo Kim <rootwarp@gmail.com>
2022-05-18 08:04:13 +0000

    WIP: Apply SDK

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"log"
 
+	pluginpb "github.com/dsrvlabs/vatz-proto/plugin/v1"
 	"github.com/dsrvlabs/vatz/sdk"
 	"golang.org/x/net/context"
 	"google.golang.org/protobuf/types/known/structpb"
 
 	"github.com/dsrvlabs/vatz-plugin-matic/policy"
+)
+
+const (
+	pluginName = "vatz-plugin-matic"
 )
 
 var (
@@ -20,7 +25,7 @@ func init() {
 
 func main() {
 	ctx := context.Background()
-	p := sdk.NewPlugin()
+	p := sdk.NewPlugin(pluginName)
 
 	p.Register(pluginFeature)
 
@@ -29,28 +34,82 @@ func main() {
 	}
 }
 
-func pluginFeature(info, option map[string]*structpb.Value) error {
+func pluginFeature(info, option map[string]*structpb.Value) (sdk.CallResponse, error) {
 	log.Println("Execute pluginFeature")
 
 	log.Println(info)
 
+	resp := sdk.CallResponse{}
+
 	val, ok := info["execute_method"]
 	if !ok {
 		log.Println("No mandatory field")
-		return nil
+		return resp, nil
 	}
 
 	switch val.GetStringValue() {
-	case "IsBorUp":
+	case "isBorUp":
 		isUp, err := executor.IsBorUp()
 		if err != nil {
-			return err
+			resp.Message = err.Error()
+			return resp, err
 		}
-		_ = isUp
+
+		log.Println(isUp, err)
+
+		if isUp {
+			resp.Severity = pluginpb.SEVERITY_INFO
+			resp.State = pluginpb.STATE_SUCCESS
+			resp.Message = "up"
+		} else {
+			resp.Severity = pluginpb.SEVERITY_CRITICAL
+			resp.State = pluginpb.STATE_FAILURE
+			resp.Message = "down"
+		}
+
+	case "isHeimdallUp":
+		isUp, err := executor.IsHeimdallUp()
+		if err != nil {
+			resp.Message = err.Error()
+			return resp, err
+		}
+
+		log.Println(isUp, err)
+		if isUp {
+			resp.Severity = pluginpb.SEVERITY_INFO
+			resp.State = pluginpb.STATE_SUCCESS
+			resp.Message = "up"
+		} else {
+			resp.Severity = pluginpb.SEVERITY_CRITICAL
+			resp.State = pluginpb.STATE_FAILURE
+			resp.Message = "down"
+		}
+
+	case "isHeimdallRestUp":
+		isUp, err := executor.IsHeimdallRestUp()
+		if err != nil {
+			resp.Message = err.Error()
+			return resp, err
+		}
+
+		log.Println(isUp, err)
+		if isUp {
+			resp.Severity = pluginpb.SEVERITY_INFO
+			resp.State = pluginpb.STATE_SUCCESS
+			resp.Message = "up"
+		} else {
+			resp.Severity = pluginpb.SEVERITY_CRITICAL
+			resp.State = pluginpb.STATE_FAILURE
+			resp.Message = "down"
+		}
 	default:
 		log.Println("No matching function")
+
+		resp.Severity = pluginpb.SEVERITY_ERROR
+		resp.State = pluginpb.STATE_FAILURE
+		resp.Message = "no matching function"
 		break
 	}
 
-	return nil
+	return resp, nil
 }
